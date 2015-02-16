9de565a5240fa38faad51fa3da9112ee9679b090
Elliott Mahler <join.together@gmail.com>
2015-02-16 13:39:57 -0800

    Added a main menu scene

---
+++ Assets/My Assets/Scripts/UI/MainMenu/ImageCycle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ImageCycle : MonoBehaviour {
+
+    [SerializeField]
+    private List<Image> images;
+
+    [SerializeField]
+    private float cycleTime;
+
+    [SerializeField]
+    private float transitionTime;
+
+    private int currentImage = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        images[currentImage].color = Color.white;
+
+        InvokeRepeating("NextImage", cycleTime, cycleTime);
+	}
+	
+	private void NextImage()
+    {
+        int previousImage = currentImage;
+        currentImage = (currentImage + 1) % images.Count;
+        int nextImage = currentImage;
+
+        //crossfade the two images
+        LeanTween.alpha(images[previousImage].rectTransform, 0.0f, transitionTime).setEase(LeanTweenType.easeInQuad);
+        LeanTween.alpha(images[nextImage].rectTransform, 1.0f, transitionTime).setEase(LeanTweenType.easeOutQuad);
+    }
+}
---
+++ Assets/My Assets/Scripts/UI/MainMenu/MenuNavigation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuNavigation : MonoBehaviour {
+
+	public void CompetitionClicked()
+    {
+        Application.LoadLevel("CompetitionMode");
+    }
+
+    public void SandboxClicked()
+    {
+        Application.LoadLevel("SandboxMode");
+    }
+}
